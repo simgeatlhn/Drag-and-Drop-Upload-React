babe76b15dc447b8b43f7543e5e94b19af96e251
simgeatlhn <simgeeatlihan@gmail.com>
2022-05-18 22:38:18 +0300

    Drag-and-Drop-Upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import DropFileInput from './components/drop-file-input/DropFileInput';
+
+function App() {
+
+  const onFileChange = (files) => {
+    console.log(files)
+  }
+
+  return (
+    <div className="box">
+      <h2 className='header'>
+        React Drag and Drop Upload
+      </h2>
+      <DropFileInput
+         onFileChange ={(files) => onFileChange(files)}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config/ImageConfig.js
@@ -0,0 +1,9 @@
+import swiftui from "../assets/swiftui.jpeg"
+import react from "../assets/react.png"
+import examplepdf from "../assets/example.pdf"
+
+export const ImageConfig = {
+    default: swiftui,
+    png:react,
+    pdf:examplepdf
+}
